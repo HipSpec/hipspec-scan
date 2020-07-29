5baed4cd9d13d4340bc5ba60b12a631ce55395e2
Mason Hensley <mason@hipspec.com>
2020-07-29 16:37:43 -0400

    docs updating

--- repo-scan.rb
+++ repo-scan.rb
@@ -5,7 +5,7 @@ require 'net/http'
 puts "GITHUB_REPO: #{ENV['GITHUB_REPOSITORY']}"
 puts "GITHUB_SHA: #{ENV['GITHUB_SHA']}"
 
-g = Git.open('../')
+g = Git.open('./')
 data = g.grep('HIPSPEC').to_json
 puts 'Writing scan to output file: hipspec-data.json'
 File.write('./hipspec-data.json', data)
