58deb7d46adcfad16a561b72fe22a78c6c476f23
Mason Hensley <mason@hipspec.com>
2020-07-29 16:09:48 -0400

    testing external action - v1

--- repo-scan.rb
+++ repo-scan.rb
@@ -4,7 +4,7 @@ require 'git'
 puts "GITHUB_REPO: #{ENV['GITHUB_REPOSITORY']}"
 puts "GITHUB_SHA: #{ENV['GITHUB_SHA']}"
 
-g = Git.open('../')
+g = Git.open('./')
 data = g.grep('HIPSPEC').to_json
 
 puts 'env'
